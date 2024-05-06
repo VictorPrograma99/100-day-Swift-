845c66ec0db3db1c1f46eaeb7228b2ed7c59cfe3
Victor <yoprogramo2024@gmail.com>
2024-05-05 20:16:21 -0500

    Initial Commit

---
+++ 1st Playground.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+var nombre = "mifoto.jpg"
+
+print(nombre.hasSuffix(".jpg"))
+ 
+var numero = 120.isMultiple(of: 10)
+print(numero)
+
+
+var tring = "esta " + "CADENA " + "DE CARACTERES"
+
+print(tring)
+
+var tring1 = """
+Esta Cadena
+Se respeta
+"""
+
+var tring2 = "The god \"believe\" hola"
+print(tring2)
+ 
+
---
+++ 1st Playground.playground/Pages/checkpoint 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let Temp = 30
+
+
+var celcius = (Temp * 9)/5 + 32
+
+print("temperatura F \(Temp) Temperatura Celcius \(celcius) ")
