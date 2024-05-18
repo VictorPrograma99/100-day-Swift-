7375c34f880c5a4dbcebacf25349247ec2864c8a
Victor <yoprogramo2024@gmail.com>
2024-05-18 07:13:57 -0500

    Just create a checkpoint page where you will find the exercises solved to practice what you learned each day.

---
+++ 1st Playground.playground/Pages/CheckPoints.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+
+//CHECKPOINT 4
+enum errores:Error {
+    case noroot , outofbones
+}
+
+
+func RaizCuadrada(_ Nu: Int) throws -> String {
+    var raiz = 0
+    if Nu > 10000 || Nu < 1 {
+        throw errores.outofbones
+    } else {
+        for i in 1...10000 {
+            if (i * i) == Nu {
+                
+                return "Raiz cuadrada es \(i)"
+            }
+        }
+        if raiz == 0{
+            throw errores.noroot
+        }else{
+            return "No se halló raiz cuadrada"}
+    }
+    
+}
+
+var number = 100
+do{
+   try print(RaizCuadrada(number))
+   
+}catch errores.outofbones{
+    print("the number is out of bones")
+}catch errores.noroot{
+    print("Number has not root")
+}
+
+
+
